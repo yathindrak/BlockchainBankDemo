2f7c458b0481cb4e7d6e6bc5448992f3c9db94fd
yathindra <yathindra.rawya@pearson.com>
2020-05-20 18:31:14 +0530

    initial commit

---
+++ Bank.sol
@@ -0,0 +1,26 @@
+pragma solidity 0.6.6;
+
+contract Bank
+{
+    int bal;
+    
+    constructor() public
+    {
+        bal = 1;
+    }
+    
+    function getBalance() view public returns(int)
+    {
+        return bal;
+    }
+    
+    function withdraw(int amt) public
+    {
+        bal = bal - amt;
+    }
+    
+    function deposit(int amt) public
+    {
+        bal = bal + amt;
+    }
+}
